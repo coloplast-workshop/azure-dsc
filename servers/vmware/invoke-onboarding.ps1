4ccabe0edb4fa559e111e85e1d40d4823a0d37a8
Lars Krogh Paulsen <lars.krogh.paulsen@atea.dk>
2021-12-12 20:54:43 +0100

    Minor update

---
+++ servers/vmware/invoke-onboarding.ps1
@@ -0,0 +1,26 @@
+﻿$resourceGroup = 'azure-dsc-rg'
+$automationAccount = 'automation-account'
+$outputFolderString = $env:SystemDrive + '\DSC'
+$params = @{
+  ResourceGroupName     = $resourceGroup
+  AutomationAccountName = $automationAccount
+  ComputerName          = @('localhost')
+  OutputFolder          = $outputFolderString
+}
+
+if ($null -ne (Get-InstalledModule -Name 'AzureRM' -ErrorAction SilentlyContinue))
+{
+  Uninstall-Module -Name 'AzureRM' -AllVersions -WarningAction Ignore
+}
+if (-not (Get-InstalledModule -Name 'Az' -ErrorAction SilentlyContinue))
+{
+  Install-Module -Name 'Az' -Force -AllowClobber
+  Uninstall-AzureRm -WarningAction Ignore
+}
+Connect-AzAccount
+Get-AzAutomationDscOnboardingMetaconfig @params -Force
+Start-Service -Name 'WinRM' -ErrorAction SilentlyContinue
+Set-DscLocalConfigurationManager -Path ('{0}\\MetaConfigs' -f $outputFolderString)
+
+# Validating node enrollment
+#Get-DscLocalConfigurationManager
